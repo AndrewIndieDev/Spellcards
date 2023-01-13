93737b8fd7d25e080a5ba40766347a3ef8dec1ba
AndrewIndieDev <andrewindiedev@gmail.com>
2023-01-13 19:14:53 +1000

    Added card manipulation
    +card movement

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SlamJam2023/CardData", fileName = "new CardData")]
+public class CardData : ScriptableObject
+{
+    public Material cardImage;
+    public Material cardBackground;
+    [TextArea]
+    public string description;
+    public Effect[] effects;
+}
