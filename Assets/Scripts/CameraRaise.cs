a09b2a83d698205149d7ef848ac9e5d127b850a6
AndrewIndieDev <andrewindiedev@gmail.com>
2023-01-16 12:40:29 +1000

    updates

--- Assets/Scripts/CameraRaise.cs
+++ Assets/Scripts/CameraRaise.cs
@@ -14,11 +14,14 @@ public class CameraRaise : MonoBehaviour
     public Camera cam;
     [Range(0,100)]
     public int screenPercentageToLookUp = 10;
+    public int screenPercentageToLookRight = 95;
 
     private float screenLookUp;
+    private float screenLookRight;
     private Tween rotTween;
     private Tween posTween;
-    private bool lookup;
+    private bool lookUp;
+    private bool lookRight;
 
     private void Start()
     {
@@ -31,21 +34,39 @@ public class CameraRaise : MonoBehaviour
         if (!GameManager.Instance.playing) return;
 
         screenLookUp = Screen.height - (Screen.height * ((float)screenPercentageToLookUp / 100));
+        screenLookRight = Screen.width - (Screen.width * ((float)screenPercentageToLookRight / 100));
 
-        if (Input.mousePosition.y > screenLookUp && !lookup)
+        if (Input.mousePosition.y > screenLookUp && !lookUp)
         {
+            ResetAllCameraProperties();
             SetCameraRotation(new Vector3(15f, 0f, 0f));
             SetCameraPosition(new Vector3(0f, 1.95f, -1.57f));
-            lookup = true;
             screenPercentageToLookUp = 90;
+            lookUp = true;
         }
-        else if (Input.mousePosition.y < screenLookUp && lookup)
+        
+        else if (Input.mousePosition.y < screenLookUp && lookUp)
         {
+            ResetAllCameraProperties();
             SetCameraRotation(new Vector3(60f, 0f, 0f));
             SetCameraPosition(new Vector3(0f, 1.5f, -.35f));
-            lookup = false;
             screenPercentageToLookUp = 10;
+            lookUp = false;
         }
+
+        //if (Input.mousePosition.x > screenLookRight && !lookRight)
+        //{
+        //    ResetAllCameraProperties();
+        //    SetCameraRotation(new Vector3(0f, 90f, 0f));
+        //    SetCameraPosition(new Vector3(0f, .8f, -1f));
+        //    lookRight = true;
+        //}
+    }
+
+    private void ResetAllCameraProperties()
+    {
+        lookRight = false;
+        lookUp = false;
     }
 
     private void SetCameraRotation(Vector3 rot)
@@ -74,5 +95,7 @@ public class CameraRaise : MonoBehaviour
     {
         SetCameraRotation(new Vector3(-90f, 0f, 0f));
         SetCameraPosition(new Vector3(0f, -.8f, -1f));
+        lookUp = false;
+        lookRight = false;
     }
 }
